d7be5cbba598d74e3429cc956ad109d1a08221b8
gandi0330 <gandi0330@naver.com>
2022-08-23 23:02:08 +0900

    20220823

---
+++ algorithm/src/Main_25304.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_25304 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int costAll = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int cost = Integer.parseInt(st.nextToken());
+            int cnt = Integer.parseInt(st.nextToken());
+
+            sum += cost*cnt;
+
+        }
+
+        if(costAll == sum){
+            System.out.println("Yes");
+        }else System.out.println("No");
+
+    }
+}
