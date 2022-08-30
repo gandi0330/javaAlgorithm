51366ddb5eaa1f4d1821eda08bc655cab399bd9e
Wise Programmer <67681207+gandi0330@users.noreply.github.com>
2022-08-30 23:51:56 +0900

    2022-08-30

---
+++ algorithm/src/구현/Main_10156.java
@@ -0,0 +1,21 @@
+package 구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_10156 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        System.out.println(K*N - M < 0 ? 0 : K*N - M);
+
+
+    }
+}
