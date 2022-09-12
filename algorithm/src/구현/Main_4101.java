e91125c96431b32bf1fe41b472e763a9a6531425
Wise Programmer <67681207+gandi0330@users.noreply.github.com>
2022-09-12 23:24:35 +0900

    2022-09-12

---
+++ algorithm/src/구현/Main_4101.java
@@ -0,0 +1,30 @@
+package 구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_4101 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            StringTokenizer st =  new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a==0 && b==0) break;
+
+            if(a > b) sb.append("Yes\n");
+            else sb.append("No\n");
+        }
+
+        System.out.print(sb);
+
+
+
+
+    }
+}
